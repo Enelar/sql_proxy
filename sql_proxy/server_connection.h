43bb9a811c4c690006169fa59f74befaf49bd8a9
Kirill Berezin <enelar@develop-project.ru>
2014-11-17 06:20:52 +0300

    Server connection draft

--- sql_proxy/server_connection.h
+++ sql_proxy/server_connection.h
@@ -1,7 +1,12 @@
 #pragma once
 
-#include "connection_handler.h"
+#include <map>
+#include "connection_async_io.h"
 
-class server_connection : connection_handler
+class server_connection : connection_async_io
 {
+  map<int, string> answers;
+  int cur_task = 0, cur_id = 0;
+public:
+  void AskSomething(string question, function<void(string)> callback);
 };
