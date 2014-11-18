86d6a0cbe1c61bab35458f5d08f6913ea14775b5
Kirill Berezin <enelar@develop-project.ru>
2014-11-18 06:27:32 +0300

    I am really shame. Restore system locale.

--- sql_proxy/debug_output.cpp
+++ sql_proxy/debug_output.cpp
@@ -4,12 +4,24 @@ dout_startup_type dout;
 
 #include <windows.h>
 
+namespace
+{
+  struct A
+  {
+    A()
+    { // Fix russian characters in russian console with russian way
+      setlocale(0, "");
+    }
+  } crappy_hacky_lammy_shame_thing;
+};
+
 template<>
 auto operator<<(dout_type &s, const wstring &obj)->dout_type &
 {
   // Could attach at any time
   if (IsDebuggerPresent())
     OutputDebugStringW(obj.c_str());
+  wcout << obj;
 
   return s;
 }
@@ -19,6 +31,7 @@ auto operator<<(dout_type &s, const string &obj)->dout_type &
 {
   if (IsDebuggerPresent())
     OutputDebugStringA(obj.c_str());
+  cout << obj;
   return s;
 }
 
